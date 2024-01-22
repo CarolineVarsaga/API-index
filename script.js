5648ea0a79fdc7c782fd53080ab6e616bfafb558
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-01-22 09:49:51 +0100

    codealong api front end headless

---
+++ script.js
@@ -0,0 +1,75 @@
+let userForm = document.querySelector("#userForm");
+let userList = document.querySelector("#userList");
+
+if (localStorage.getItem("user")) {
+    printLogoutBtn();
+} else {
+    printLoginForm();
+}
+
+function printUsers() {
+    fetch("http://localhost:3002/users")
+    .then(res => res.json())
+    .then(data => {
+        console.log("data", data);
+
+        data.map(user => {
+            let li = document.createElement("li");
+            li.innerText = user.name;
+
+            userList.appendChild(li);
+        })
+    })
+}
+
+printUsers();
+
+function printLoginForm() {
+    userForm.innerHTML = "";
+    let inputEmail = document.createElement("input");
+    inputEmail.placeholder = "Epost";
+    let inputPassword = document.createElement("input");
+    inputPassword.placeholder = "Lösenord"; 
+    inputPassword.type = "password";
+    let loginBtn = document.createElement("button"); 
+    loginBtn.innerText = "Logga in"; 
+
+    loginBtn.addEventListener("click", () => {
+        let sendUser = {email: inputEmail.value, password: inputPassword.value };
+
+        fetch("http://localhost:3002/login", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sendUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log("post user", data);
+ 
+            if (data.user) {
+                localStorage.setItem("user", data.user)
+                printLogoutBtn();
+            } else {
+                alert("Fel inlogg");
+            }
+        })
+    })
+
+    userForm.append(inputEmail, inputPassword, loginBtn);
+}
+
+function printLogoutBtn() {
+    userForm.innerHTML = "";
+
+    let logoutBtn = document.createElement("button");
+    logoutBtn.innerText = "Logga ut";
+
+    logoutBtn.addEventListener("click", () => {
+        localStorage.removeItem("user");
+        printLoginForm();
+    })
+
+    userForm.appendChild(logoutBtn);
+}
